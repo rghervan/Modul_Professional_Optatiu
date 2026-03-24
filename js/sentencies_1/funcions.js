1392e25bdb433abc2eab857f99c832b2c5b2f8e4
rghervan <rghervan@inscaparrella.cat>
2026-03-24 16:00:21 +0100

    function

--- js/sentencies_1/funcions.js
+++ js/sentencies_1/funcions.js
@@ -13,7 +13,7 @@ console.log(missatge2);
 */
 //Parts d'una funció
 //entrades(arguments), codi, sortida(return)
-
+/*
 function quadratNumero(num){
     var resultat = num * num;
     return resultat;
@@ -40,7 +40,7 @@ var c = quadratNumero(quadratNumero(numero));
 console.log(c);
 var suma = 4 + multiplicarNums(7,4);
 console.log (suma);
-
+*/
 //var valor = quadratNumero(numero);
 //console.log(valor);
 //console.log(quadratNumero(5));
@@ -49,29 +49,31 @@ console.log (suma);
  // 32F = 0C, 68F = 20C.
  //C = (F - 32) * 5/9
 /*
-function convertirFahrenheitACelsius(grausFah){
+function convertirFahrenheitACelsius(grausFah,parama,paramb){
     var celsius = (grausFah - 32) * 5 / 9;
+    console.log(parama);
     return celsius;
 }
 
-var tempUn = convertirFahrenheitACelsius(32);
-var tempDos = convertirFahrenheitACelsius(68);
+var tempUn = convertirFahrenheitACelsius(32,70,20);
+var tempDos = convertirFahrenheitACelsius(68,80);
 console.log(tempUn);
 console.log(tempDos);
-*/
 
-/*function calcularEdat(anyNaixement){
+*/
+/*
+function calcularEdat(anyNaixement){
     return 2019 - anyNaixement;
 }
-
+/*
 var edat1 = calcularEdat(1990);
 var edat2 = calcularEdat(1980);
 var edat3 = calcularEdat(1970);
 console.log(edat1);
 console.log(edat2);
 console.log(edat3);
-
-/****************
+*/
+/*
  * Exercici de codificació 3
  * 
  * Calcular quants anys li falten a una persona per jubilar-se.
@@ -79,9 +81,9 @@ console.log(edat3);
  * Enviar com a dades a la funció el seu any de naixement i el seu nom.
  * 
  * Solució:
- */
+*/
 
- /*function calcularTempsJubilacio(anyNaixement, nom){
+/* function calcularTempsJubilacio(anyNaixement, nom){
      var edat = calcularEdat(anyNaixement);
      var anysJubilacio = 65 - edat;
      console.log(nom + ' li falten ' + anysJubilacio + ' anys per a la seva jubilació.');
@@ -89,14 +91,14 @@ console.log(edat3);
 
  calcularTempsJubilacio(1990,'Pau');
  calcularTempsJubilacio(1978, 'Josep');
-*/
+
 
 /********************
  * Funcions com a expressions
  */
 //Arguments Undefined
 /*var nom; 
-var prova = function(n){
+var prova = function(n){ //---->Equivalent a Function prova(n)
     return 'Hola ' + n;
 }
 nom = 'Pau';
@@ -113,11 +115,12 @@ console.log(valorNul(a));
 */
 
 //Arguments per defecte
-
-/*var sumar = function(a = 5, b = 3, c = 3){
+/*
+var sumar = function(a = 5, b = 3, c = 3){
     return a + b + c;
 }
-console.log(sumar());
+console.log(sumar(1,3,5));
+console.log(sumar(10));
 
 //Plantilles de cadenes (template string)
 var nom = 'Pau';
@@ -126,8 +129,15 @@ console.log(`El nom és: ${nom}`);
 var a = 5;
 var b = 10;
 //console.log('La suma és:' + (a + b));
-console.log(`La suma és: ${a + b}`);*/
-
+console.log(`La suma és: ${a + b}`);
+*/
+/*
+function factorial(n)
+{
+    if (n==1) return 1;
+    else return n*factorial(n-1);
+}
+console.log (factorial (5))
 
 /*
  * Exercici 4
@@ -146,8 +156,9 @@ console.log(`La suma és: ${a + b}`);*/
  */
 /*
  var calcularPuntuacio = function(nom, pos, neg){
-     var percentPos = (pos / 100) * 100;
-     var percentNeg = (neg / 100) * 100;
+    var suma = pos + neg; 
+    var percentPos = (pos / suma) * 100;
+     var percentNeg = (neg / suma) * 100;
      var puntuacio = '';
 
      if(percentPos > 90){
@@ -161,8 +172,8 @@ console.log(`La suma és: ${a + b}`);*/
      }
      return `${nom} té la puntuació ${puntuacio}, Positives:${percentPos}%, Negatives:${percentNeg}% `;     
  }
-*/
-/*
- var resultat = calcularPuntuacio('Pau', 75, 25);
+
+
+ var resultat = calcularPuntuacio('Pau', 200,100 );
  console.log(resultat);
- */
+*/
